241fad26f4df52c50ef27c29ce9267245d41f526
s-khechnev <hechnev@gmail.com>
2025-11-18 19:32:53 +0300

    Add users's repo

--- internal/storage/users/postgres.go
+++ internal/storage/users/postgres.go
@@ -2,6 +2,8 @@ package users
 
 import (
 	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	usersDomain "reviewer-assigner/internal/domain/users"
 )
@@ -15,6 +17,36 @@ func NewPostgresUserRepository(pool *pgxpool.Pool) *PostgresUserRepository {
 }
 
 func (r *PostgresUserRepository) Get(ctx context.Context, userID string) (*usersDomain.User, error) {
+	const query = `
+	SELECT u.external_id, u.name, u.is_active, u.team_name FROM users u WHERE external_id = $1
+	`
 
-	return nil, nil
+	rows, _ := r.pool.Query(ctx, query, userID)
+	user, err := pgx.CollectExactlyOneRow(rows, pgx.RowToAddrOfStructByName[usersDomain.User])
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (r *PostgresUserRepository) SetIsActive(ctx context.Context, userID string, isActive bool) (*usersDomain.User, error) {
+	const query = `
+        WITH updated_user AS (
+            UPDATE users 
+            SET is_active = $1 
+            WHERE external_id = $2
+            RETURNING external_id, name, is_active, team_name
+        )
+        SELECT external_id, name, is_active, team_name
+        FROM updated_user
+    `
+
+	rows, _ := r.pool.Query(ctx, query, userID, isActive)
+	user, err := pgx.CollectExactlyOneRow(rows, pgx.RowToAddrOfStructByName[usersDomain.User])
+	if err != nil {
+		return nil, fmt.Errorf("failed to update user: %w", err)
+	}
+
+	return user, nil
 }
