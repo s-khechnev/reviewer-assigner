c67b22cb9a26e230289558deef1a1920f5f59ac1
s-khechnev <hechnev@gmail.com>
2025-11-19 04:25:16 +0300

    Polishing user's repo

--- internal/storage/users/postgres.go
+++ internal/storage/users/postgres.go
@@ -3,7 +3,6 @@ package users
 import (
 	"context"
 	"fmt"
-	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	usersDomain "reviewer-assigner/internal/domain/users"
 )
@@ -13,40 +12,42 @@ type PostgresUserRepository struct {
 }
 
 func NewPostgresUserRepository(pool *pgxpool.Pool) *PostgresUserRepository {
-	return &PostgresUserRepository{pool: pool}
+	return &PostgresUserRepository{
+		pool: pool,
+	}
 }
 
 func (r *PostgresUserRepository) Get(ctx context.Context, userID string) (*usersDomain.User, error) {
 	const query = `
-	SELECT u.external_id, u.name, u.is_active, u.team_name FROM users u WHERE external_id = $1
+	SELECT u.user_id, u.name, u.is_active, t.name FROM users u
+	JOIN teams t ON t.id = u.team_id
+	WHERE user_id = $1
 	`
 
-	rows, _ := r.pool.Query(ctx, query, userID)
-	user, err := pgx.CollectExactlyOneRow(rows, pgx.RowToAddrOfStructByName[usersDomain.User])
+	var user usersDomain.User
+	err := r.pool.
+		QueryRow(ctx, query, userID).
+		Scan(&user.ID, &user.Name, &user.IsActive, &user.TeamName)
 	if err != nil {
 		return nil, err
 	}
 
-	return user, nil
+	return &user, nil
 }
 
 func (r *PostgresUserRepository) SetIsActive(ctx context.Context, userID string, isActive bool) (*usersDomain.User, error) {
 	const query = `
-        WITH updated_user AS (
-            UPDATE users 
-            SET is_active = $1 
-            WHERE external_id = $2
-            RETURNING external_id, name, is_active, team_name
-        )
-        SELECT external_id, name, is_active, team_name
-        FROM updated_user
+    UPDATE users 
+    SET is_active = $1 
+    WHERE user_id = $2
+    RETURNING user_id, name, is_active, (SELECT name FROM teams WHERE id = team_id) as team_name
     `
 
-	rows, _ := r.pool.Query(ctx, query, userID, isActive)
-	user, err := pgx.CollectExactlyOneRow(rows, pgx.RowToAddrOfStructByName[usersDomain.User])
+	var user usersDomain.User
+	err := r.pool.QueryRow(ctx, query, userID, isActive).Scan(&user.ID, &user.Name, &user.IsActive, &user.TeamName)
 	if err != nil {
 		return nil, fmt.Errorf("failed to update user: %w", err)
 	}
 
-	return user, nil
+	return &user, nil
 }
